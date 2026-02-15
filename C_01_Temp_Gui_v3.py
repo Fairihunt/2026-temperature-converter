002f4cac029a0fac4a7165882bf0b2ad57bcc9fa
Venus <guanv2051@masseyhigh.school.nz>
2026-02-16 09:21:54 +1300

    Complete temperature GUI

--- C_01_Temp_Gui_v3.py
+++ C_01_Temp_Gui_v3.py
@@ -1,8 +1,6 @@
 from tkinter import *
 import all_constants as c
 
-
-
 class Converter():
     """
     Temperature conversion tool (°C to °F or °F to °C
@@ -19,7 +17,7 @@ class Converter():
 
         self.temp_heading = Label(self.temp_frame,
                                   text="Temperature Convertor",
-                                  font=("Arial", "16", "bold")
+                                  font=("Arial", 16, "bold")
                                   )
         self.temp_heading.grid(row=0)
 
@@ -33,7 +31,7 @@ class Converter():
         self.temp_instructions.grid(row=1)
 
         self.temp_entry = Entry(self.temp_frame,
-                                font=("Arial", "14")
+                                font=("Arial", 14)
                                 )
         self.temp_entry.grid(row=2, padx=10, pady=10)
 
@@ -48,8 +46,8 @@ class Converter():
 
         # button list (button text | bg colour | command | row | column)
         button_details_list = [
-            ["To Celsius", "#990099", lambda:self.check_temp(c.ABS_ZERO_FAHRENHEIT)],
-            ["To Fahrenheit", "#009900", "", 0, 1],
+            ["To Celsius", "#990099", lambda:self.check_temp(c.ABS_ZERO_FAHRENHEIT), 0, 0],
+            ["To Fahrenheit", "#009900", lambda:self.check_temp(c.ABS_ZERO_CELSIUS), 0, 1],
             ["Help / Info", "#CC6600", "", 1, 0],
             ["History / Export", "#004C99", "", 1, 1]
         ]
@@ -60,7 +58,7 @@ class Converter():
         for item in button_details_list:
             self.make_button = Button(self.button_frame,
                                       text=item[0], bg=item[1],
-                                      fg="#FFFFFF", font=("Arial", "12", "bold" ),
+                                      fg="#FFFFFF", font=("Arial", 12, "bold" ),
                                       width=12, command=item[2])
             self.make_button.grid(row=item[3], column=item[4], padx=5, pady=5)
 
@@ -69,13 +67,24 @@ class Converter():
         # retrieve 'history / export' button and disable it at the start
         self.to_history_button = self.button_ref_list[3].config(state=DISABLED)
 
+    def check_temp(self,min_temp):
+        print("Min Temp:", min_temp)
+
+        # Retrieve temperature to be converted
+        to_convert = self.temp_entry.get()
+        print("to convert", to_convert)
+
+        try:
+            to_convert = float(to_convert)
+            if to_convert >= min_temp:
+                print("You are OK")
+            else:
+                print("Too Low!")
 
 
 
 
-        # main routine
-
-
+# main routine
 if __name__ ==  "__main__":
     root = Tk()
     root.title("Temperature Converter")
